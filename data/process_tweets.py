5bc755e3dd4752206f2951297f1c215ceae58131
MTMoeller <mmoeller@andrew.cmu.edu>
2022-10-26 21:32:46 -0400

    process_tweets updated and nagorno2 dataset created

--- data/process_tweets.py
+++ data/process_tweets.py
@@ -1,17 +1,23 @@
 import pandas as pd
-import urllib.parse as urlparse
-import sys
+import click
 from pathlib import Path
 
+@click.command()
+@click.argument('filename', type=click.Path(exists=True, path_type=Path))
+def process_tweet(filename):
+    file = filename
+    path = Path.cwd()
+    data_Dir = path.joinpath('data')
+    file_Dir = path.joinpath(filename)
+    tweets = pd.read_csv(file_Dir, dtype='str') # This is a dataframe of actors on twitter and their information
+    tweets.head(10)
+    processed = filename.stem + "_processed.csv"
+    out_path = data_Dir.joinpath(processed)
+    print(out_path)
+    tweets_processed = tweets.loc[:, ['id', 'text']]
+    tweets_processed.to_csv(out_path, header=False, index=False)
+    print("Tweets processed")
 
-tweets_filename = sys.argv[1]
-path = Path.cwd()
-path_root = path.parent
-data_Dir = path_root.joinpath('data')
-file_Path = data_Dir.joinpath("ukraine.csv")
-print(file_Path)
-tweets = pd.read_csv(file_Path) # This is a dataframe of actors on twitter and their information
-tweets.head(10)
-out_path = data_Dir.joinpath("ukraine_processed.csv")
-tweets_processed = tweets.loc[:, ['id', 'text']]
-tweets_processed.to_csv(out_path, header=False, index=False)
+if __name__ == "__main__":
+    process_tweet()
+    ##  python data/process_tweets.py data/nagorno2.csv  Ran this to make this happen
